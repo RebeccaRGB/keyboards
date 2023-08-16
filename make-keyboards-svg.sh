c6e19342a385b30275c006cf19714efded68df24
Rebecca G. Bettencourt <beckiejo@gmail.com>
2023-08-16 00:12:36 -0700

    Update build script and remove debug output

--- make-keyboards-svg.sh
+++ make-keyboards-svg.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 rm keyboards-svg/*.svg
-cd keyboards
-for i in *.txt
-do java -jar ../java/KeyCaps/keycaps.jar -f "$i" -o "../keyboards-svg/${i%.*}.svg"
-done
+java -jar java/KeyCaps/keycaps.jar -o keyboards-svg -- keyboards/*.txt
