94b4aa811f70a9075f242164845f816a861e6306
Rebecca G. Bettencourt <beckiejo@gmail.com>
2023-11-20 02:04:52 -0800

    Update LayoutToSVG

--- java/KeyCaps/src/com/kreative/keycaps/LayoutToSVG.java
+++ java/KeyCaps/src/com/kreative/keycaps/LayoutToSVG.java
@@ -13,129 +13,163 @@ import java.util.Scanner;
 public final class LayoutToSVG {
 	public static void main(String[] args) throws IOException {
 		boolean parseOpts = true;
-		File outputFile = null;
+		KeyCapMold mold = new IconKeyCapMold();
+		float moldScale = 1;
+		float size = 48;
+		File output = null;
 		boolean test = false;
+		
 		int argi = 0;
 		while (argi < args.length) {
 			String arg = args[argi++];
 			if (parseOpts && arg.startsWith("-")) {
 				if (arg.equals("--")) {
 					parseOpts = false;
+				} else if (arg.equals("--help")) {
+					help();
+					return;
+				} else if (arg.equals("-m") && argi < args.length) {
+					arg = args[argi++];
+					mold = KeyCapMold.forName(arg);
+					if (mold == null) {
+						System.err.println("Unknown mold: " + arg);
+						return;
+					}
+				} else if (arg.equals("-s") && argi < args.length) {
+					arg = args[argi++];
+					try { moldScale = Float.parseFloat(arg); }
+					catch (NumberFormatException e) {
+						System.err.println("Invalid scale: " + arg);
+						return;
+					}
+				} else if (arg.equals("-u") && argi < args.length) {
+					arg = args[argi++];
+					try { size = Float.parseFloat(arg); }
+					catch (NumberFormatException e) {
+						System.err.println("Invalid size: " + arg);
+						return;
+					}
 				} else if (arg.equals("-i")) {
-					KeyCapLayout kcl = read(System.in);
-					if (outputFile == null) {
-						write(System.out, kcl, test);
-					} else if (outputFile.isDirectory()) {
-						String outputName = "out.svg";
-						OutputStream out = new FileOutputStream(new File(outputFile, outputName));
-						write(out, kcl, test);
-						out.close();
-					} else {
-						OutputStream out = new FileOutputStream(outputFile);
-						write(out, kcl, test);
-						out.close();
-						outputFile = null;
-					}
+					KeyCapLayout layout = read(System.in);
+					if (layout == null) return;
+					SVGRenderer svg = new SVGRenderer(mold, moldScale, size, null);
+					int res = write(null, output, svg, layout, test);
+					if (res == 0xE) return;
+					if (res == 0xF) output = null;
 				} else if (arg.equals("-f") && argi < args.length) {
-					File inputFile = new File(args[argi++]);
-					InputStream in = new FileInputStream(inputFile);
-					KeyCapLayout kcl = read(in);
-					in.close();
-					if (outputFile == null) {
-						write(System.out, kcl, test);
-					} else if (outputFile.isDirectory()) {
-						String outputName = inputFile.getName().replaceFirst("[.][Tt][Xx][Tt]$", ".svg");
-						OutputStream out = new FileOutputStream(new File(outputFile, outputName));
-						write(out, kcl, test);
-						out.close();
-					} else {
-						OutputStream out = new FileOutputStream(outputFile);
-						write(out, kcl, test);
-						out.close();
-						outputFile = null;
-					}
+					File input = new File(args[argi++]);
+					KeyCapLayout layout = read(input);
+					if (layout == null) return;
+					SVGRenderer svg = new SVGRenderer(mold, moldScale, size, null);
+					int res = write(input, output, svg, layout, test);
+					if (res == 0xE) return;
+					if (res == 0xF) output = null;
 				} else if (arg.equals("-o") && argi < args.length) {
-					outputFile = new File(args[argi++]);
+					output = new File(args[argi++]);
+				} else if (arg.equals("-p")) {
+					output = null;
 				} else if (arg.equals("-t")) {
 					test = true;
 				} else if (arg.equals("-T")) {
 					test = false;
 				} else {
-					System.err.println("Unknown option: " + arg);
+					help();
 					return;
 				}
 			} else {
-				File inputFile = new File(arg);
-				InputStream in = new FileInputStream(inputFile);
-				KeyCapLayout kcl = read(in);
-				in.close();
-				if (outputFile == null) {
-					write(System.out, kcl, test);
-				} else if (outputFile.isDirectory()) {
-					String outputName = inputFile.getName().replaceFirst("[.][Tt][Xx][Tt]$", ".svg");
-					OutputStream out = new FileOutputStream(new File(outputFile, outputName));
-					write(out, kcl, test);
-					out.close();
-				} else {
-					OutputStream out = new FileOutputStream(outputFile);
-					write(out, kcl, test);
-					out.close();
-					outputFile = null;
-				}
+				File input = new File(arg);
+				KeyCapLayout layout = read(input);
+				if (layout == null) return;
+				SVGRenderer svg = new SVGRenderer(mold, moldScale, size, null);
+				int res = write(input, output, svg, layout, test);
+				if (res == 0xE) return;
+				if (res == 0xF) output = null;
 			}
 		}
 	}
 	
-	private static KeyCapLayout read(InputStream in) throws IOException {
-		StringBuffer contents = new StringBuffer();
-		Scanner scanner = new Scanner(in, "UTF-8");
-		while (scanner.hasNextLine()) {
-			String line = scanner.nextLine().trim();
-			if (line.length() == 0 || line.startsWith("#")) continue;
-			contents.append(line);
-			contents.append("\n");
-		}
-		scanner.close();
-		String s = contents.toString();
+	private static KeyCapLayout read(File input) {
 		try {
-			KeyCapLayout kcl = new KeyCapLayout();
-			kcl.parse(s);
-			return kcl;
-		} catch (IllegalArgumentException e) {
-			System.err.println(e);
-			System.err.println(s);
-			throw new IOException(e);
+			InputStream in = new FileInputStream(input);
+			KeyCapLayout layout = read(in);
+			in.close();
+			return layout;
+		} catch (IOException e) {
+			System.err.println("Could not read from input: " + e);
+			return null;
 		}
 	}
 	
-	private static void write(OutputStream os, KeyCapLayout kcl, boolean test) throws IOException {
+	private static KeyCapLayout read(InputStream in) {
+		try {
+			StringBuffer contents = new StringBuffer();
+			Scanner scanner = new Scanner(in, "UTF-8");
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine().trim();
+				if (line.length() == 0 || line.startsWith("#")) continue;
+				contents.append(line);
+				contents.append("\n");
+			}
+			scanner.close();
+			KeyCapLayout layout = new KeyCapLayout();
+			layout.parse(contents.toString());
+			return layout;
+		} catch (IllegalArgumentException e) {
+			System.err.println("Could not parse input: " + e);
+			return null;
+		}
+	}
+	
+	private static int write(File input, File output, SVGRenderer svg, KeyCapLayout layout, boolean test) {
+		try {
+			if (output == null) {
+				write(System.out, svg, layout, test);
+				return 0x5;
+			} else if (output.isDirectory()) {
+				String name = (input == null) ? "out.svg" : input.getName().replaceFirst("[.][Tt][Xx][Tt]$", ".svg");
+				OutputStream out = new FileOutputStream(new File(output, name));
+				write(out, svg, layout, test);
+				out.close();
+				return 0xD;
+			} else {
+				OutputStream out = new FileOutputStream(output);
+				write(out, svg, layout, test);
+				out.close();
+				return 0xF;
+			}
+		} catch (IOException e) {
+			System.err.println("Could not write to output: " + e);
+			return 0xE;
+		}
+	}
+	
+	private static void write(OutputStream os, SVGRenderer svg, KeyCapLayout layout, boolean test) throws IOException {
 		PrintWriter out = new PrintWriter(new OutputStreamWriter(os, "UTF-8"), true);
-		SVGRenderer svg = new SVGRenderer(new IconKeyCapMold(), 1, 48, null);
-		out.println(svg.render(kcl));
+		out.println(svg.render(layout));
 		if (test) {
 			out.println("<!-- toString():");
-			String s = kcl.toString();
+			String s = layout.toString();
 			out.println(s);
 			out.println("-->");
 			out.println("<!-- toNormalizedString():");
-			String n = kcl.toNormalizedString();
+			String n = layout.toNormalizedString();
 			out.println(n);
 			out.println("-->");
 			out.println("<!-- toMinimizedString():");
-			String m = kcl.toMinimizedString();
+			String m = layout.toMinimizedString();
 			out.println(m);
 			out.println("-->");
 			out.println("<!-- Tests:");
-			KeyCapLayout skcl = new KeyCapLayout(); skcl.parse(s);
-			check(out, skcl.toString(), s, "parse(s).toString() != s");
-			check(out, skcl.toNormalizedString(), n, "parse(s).toNormalizedString() != n");
-			check(out, skcl.toMinimizedString(), m, "parse(s).toMinimizedString() != m");
-			KeyCapLayout nkcl = new KeyCapLayout(); nkcl.parse(n);
-			check(out, nkcl.toNormalizedString(), n, "parse(n).toNormalizedString() != n");
-			check(out, nkcl.toMinimizedString(), m, "parse(n).toMinimizedString() != m");
-			KeyCapLayout mkcl = new KeyCapLayout(); mkcl.parse(m);
-			check(out, mkcl.toNormalizedString(), n, "parse(m).toNormalizedString() != n");
-			check(out, mkcl.toMinimizedString(), m, "parse(m).toMinimizedString() != m");
+			KeyCapLayout stdLayout = new KeyCapLayout(); stdLayout.parse(s);
+			check(out, stdLayout.toString(), s, "parse(s).toString() != s");
+			check(out, stdLayout.toNormalizedString(), n, "parse(s).toNormalizedString() != n");
+			check(out, stdLayout.toMinimizedString(), m, "parse(s).toMinimizedString() != m");
+			KeyCapLayout normLayout = new KeyCapLayout(); normLayout.parse(n);
+			check(out, normLayout.toNormalizedString(), n, "parse(n).toNormalizedString() != n");
+			check(out, normLayout.toMinimizedString(), m, "parse(n).toMinimizedString() != m");
+			KeyCapLayout minLayout = new KeyCapLayout(); minLayout.parse(m);
+			check(out, minLayout.toNormalizedString(), n, "parse(m).toNormalizedString() != n");
+			check(out, minLayout.toMinimizedString(), m, "parse(m).toMinimizedString() != m");
 			out.println("-->");
 		}
 	}
@@ -151,6 +185,17 @@ public final class LayoutToSVG {
 				out.println("+" + line);
 			}
 		}
+	}
+	
+	public static void help() {
+		System.err.println("  -m <str>      Specify mold; default IconKeyCapMold");
+		System.err.println("  -s <num>      Specify mold scaling; default 1");
+		System.err.println("  -u <num>      Specify size; default 48");
+		System.err.println("  -i            Specify standard input");
+		System.err.println("  -f <path>     Specify input file");
+		System.err.println("  -o <path>     Specify output file");
+		System.err.println("  -p            Specify standard output");
+		System.err.println("  --            Treat remaining arguments as input files");
 	}
 	
 	private LayoutToSVG() {}
