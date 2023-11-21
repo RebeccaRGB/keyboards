4c2a06f3816985026b2d6c021902ee01ae08abc8
Rebecca G. Bettencourt <beckiejo@gmail.com>
2023-11-20 23:56:01 -0800

    Switch to Max

--- make-keyboards-svg.sh
+++ make-keyboards-svg.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 rm keyboards-svg/*.svg
-java -jar java/KeyCaps/keycaps.jar -o keyboards-svg -- keyboards/*.txt
+java -jar java/KeyCaps/keycaps.jar -m max -u 54 -o keyboards-svg -- keyboards/*.txt
